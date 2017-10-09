128a57825cec1100fac3bdf74b47031d976afab8
cmf00627@vipmro.net <cmf00627@vipmro.net>
2017-10-09 10:10:45 +0800

    init

--- eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
+++ eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springcloud;
+package com.example.eureka;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringcloudApplicationTests {
+public class EurekaApplicationTests {
 
 	@Test
 	public void contextLoads() {
